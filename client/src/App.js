074ddf346f499848ccf5a70e529a3b80eb296cdb
savoryboi <andbjerk@yahoo.com>
2023-06-13 13:14:22 -0500

    client access to graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useQuery, gql } from '@apollo/client';
 import './App.css';
+import List from './components/List';
+import { GET_ALL_ITEMS, GET_ALL_CATEGORIES } from './utils/queries';
+
 
 function App() {
+  const { error, loading, data } = useQuery(GET_ALL_CATEGORIES);
+  data ? console.log(data) : console.log(error);
+
+  const categoryData = data.getAllCategories;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        categoryData.map((cat) => {
+          return (
+            <div key={cat._id} className='category_wrapper'>
+              <h1>{cat.categoryName}</h1>
+              {cat.categoryItems.map((item) => {
+                return (
+                  <div key={item._id}>
+                    <ul>
+                      <li>{item.itemName}</li>
+                    </ul>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
